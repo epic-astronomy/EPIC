474b84ee81961739b378b8d97ef2a98ae5d7e89e
Adam Beardsley <adam.p.beardsley@gmail.com>
2015-11-02 14:14:11 -0700

    Small changes to multi_source_cal
    From 9da3ea9 (branch cal_devel):
    ============
    (*) Fine tuning some parameters to make multi-source work better.
    (*) Took out functional form in multi_source_cal.py.

--- main/multi_source_cal.py
+++ main/multi_source_cal.py
@@ -11,8 +11,10 @@ import ipdb as PDB
 import MOFF_cal
 import aperture as APR
 
-cal_iter=1
-itr = 20*cal_iter
+#@profile
+#def main():
+cal_iter=500
+itr = 25*cal_iter
 rxr_noise = 0.0
 model_frac = 1.0 # fraction of total sky flux to model
 
@@ -31,13 +33,13 @@ f_center = f0
 channel_width = 40e3
 bandwidth = nchan * channel_width
 dt = 1/bandwidth
-freqs = arange(f0-nchan/2*channel_width,f0+nchan/2*channel_width,channel_width)
+freqs = NP.arange(f0-nchan/2*channel_width,f0+nchan/2*channel_width,channel_width)
 
 # ** Use this for MWA core
-#antenna_file = '/data3/t_nithyanandan/project_MWA/MWA_128T_antenna_locations_MNRAS_2012_Beardsley_et_al.txt'
+antenna_file = '/data3/t_nithyanandan/project_MWA/MWA_128T_antenna_locations_MNRAS_2012_Beardsley_et_al.txt'
 
 # ** Use this for LWA
-antenna_file = '/home/beards/inst_config/LWA_antenna_locs.txt'
+#antenna_file = '/home/beards/inst_config/LWA_antenna_locs.txt'
 
 identical_antennas = True
 ant_info = NP.loadtxt(antenna_file, skiprows=6, comments='#', usecols=(0,1,2,3))
@@ -85,14 +87,15 @@ antpos_info = aar.antenna_positions(sort=True)
 
 #### Set up sky model
 
-n_src = 1
+n_src = 5
 lmrad = NP.random.uniform(low=0.0,high=0.1,size=n_src).reshape(-1,1)**(0.5)
 lmrad[-1]=0.00
 lmang = NP.random.uniform(low=0.0,high=2*NP.pi,size=n_src).reshape(-1,1)
 #lmrad[0] = 0.0
 skypos = NP.hstack((lmrad * NP.cos(lmang), lmrad * NP.sin(lmang)))
-src_flux = NP.sort((NP.random.uniform(low=0,high=1.0,size=n_src))**4)
-#src_flux[0]=1.0
+#src_flux = NP.sort((NP.random.uniform(low=0,high=1.0,size=n_src))**(4))
+src_flux = NP.sort((NP.random.uniform(low=0.5,high=1.0,size=n_src)))
+#src_flux[-2]=0.8
 src_flux[-1]=1.0
 tot_flux=NP.sum(src_flux)
 frac_flux=0.0
@@ -100,10 +103,10 @@ ind=0
 while frac_flux < model_frac:
     ind+=1
     frac_flux=NP.sum(src_flux[-ind:])/tot_flux
-    
 
-nvect = np.sqrt(1.0-NP.sum(skypos**2, axis=1)).reshape(-1,1) 
-skypos = np.hstack((skypos,nvect))
+
+nvect = NP.sqrt(1.0-NP.sum(skypos**2, axis=1)).reshape(-1,1) 
+skypos = NP.hstack((skypos,nvect))
 
 sky_model = NP.zeros((n_src,nchan,4))
 sky_model[:,:,0:3] = skypos.reshape(n_src,1,3)
@@ -114,10 +117,10 @@ sky_model=sky_model[-ind:,:,:]
 ####  set up calibration
 calarr={}
 ant_pos = ant_info[:,1:] # I'll let the cal class put it in wavelengths.
-    
+
 for pol in ['P1','P2']:
     calarr[pol] = MOFF_cal.cal(ant_pos,freqs,n_iter=cal_iter,sim_mode=True,sky_model=sky_model,gain_factor=0.5,pol=pol,cal_method='multi_source',inv_gains=False)
-    #calarr[pol].scramble_gains(0.5) 
+    #calarr[pol].scramble_gains(0.5)
 
 # Create array of gains to watch them change
 ncal=itr/cal_iter
@@ -158,8 +161,8 @@ for i in xrange(itr):
 
         update_info['antennas'] += [adict]
 
-    aar.update(update_info, parallel=True, verbose=False, nproc=16)
-    
+    aar.update(update_info, parallel=False, verbose=False, nproc=16)
+
     ### Calibration steps
     # read in data array
     aar.caldata['P1']=aar.get_E_fields('P1',sort=True)
@@ -170,39 +173,39 @@ for i in xrange(itr):
     #amp_full_stack[i,:] = NP.abs(tempdata[0,:])**2
     # Apply calibration and put back into antenna array
     aar.caldata['P1']['E-fields'][0,:,:]=calarr['P1'].apply_cal(tempdata)
-    
+
     if grid_map_method == 'regular':
         aar.grid_convolve_new(pol='P1', method='NN',distNN=0.5*FCNST.c/f0, tol=1.0e-6,maxmatch=1,identical_antennas=True,gridfunc_freq='scale',mapping='weighted',wts_change=False,parallel=False, pp_method='queue',nproc=16, cal_loop=True,verbose=False)
     else:
         if i == 0:
-            aar.genMappingMatrix(pol=None,method='NN',distNN=0.5*NP.sqrt(ant_sizex**2+ant_sizey**2),identical_antennas=True,gridfunc_freq='scale',wts_change=False,parallel=False)
+            aar.genMappingMatrix(pol='P1',method='NN',distNN=0.5*NP.sqrt(ant_sizex**2+ant_sizey**2)/NP.sqrt(2),identical_antennas=True,gridfunc_freq='scale',wts_change=False,parallel=False)
 
     if i == 0:
         imgobj = AA.NewImage(antenna_array=aar,pol='P1')
     else:
         imgobj.update(antenna_array=aar,reset=True)
 
-    imgobj.imagr(weighting='natural',pol='P1',pad='off',verbose=False,grid_map_method=grid_map_method)
+    imgobj.imagr(weighting='natural',pol='P1',pad=0,verbose=False,grid_map_method=grid_map_method,cal_loop=True,stack=False)
 
     # update calibration
     calarr['P1'].update_cal(tempdata,imgobj,0)
-    
+
     if i == 0:
         avg_img = imgobj.img['P1'].copy()
         im_stack = NP.zeros((ncal,avg_img.shape[0],avg_img.shape[1]),dtype=NP.double)
         im_stack[cali,:,:] = avg_img[:,:,2].copy()
         temp_im = avg_img[:,:,2]
-    
+
         temp_amp = NP.abs(tempdata[0,:])**2
         gain_stack[cali,:,:] = calarr['P1'].sim_gains
         amp_stack[cali,:] = NP.abs(tempdata[0,:])**2
         cali += 1
         gain_stack[cali,:,:] = calarr['P1'].curr_gains
-    
+
     else:
         avg_img = avg_img+imgobj.img['P1'].copy()
         temp_im = temp_im+imgobj.img['P1'][:,:,2].copy()
-        
+
         temp_amp += NP.abs(tempdata[0,:])**2
         if i % cal_iter == 0:
             im_stack[cali,:,:] = temp_im/cal_iter
@@ -218,9 +221,11 @@ for i in xrange(itr):
         print 'NAN in calibration gains! exiting!'
         break
 
-avg_img /= itr
+    avg_img /= itr
+#    PDB.set_trace()
 
 
-
-
+#if __name__=='__main__':
+#    main()
+    
 
