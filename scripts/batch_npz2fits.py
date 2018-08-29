45aa103243cd5e5893905185dc1160d37520bd18
Adam Beardsley <adam.p.beardsley@gmail.com>
2018-08-29 15:00:57 -0600

    actually parse args in script

--- scripts/batch_npz2fits.py
+++ scripts/batch_npz2fits.py
@@ -6,8 +6,9 @@ import numpy as np
 a = argparse.ArgumentParser(description='Convert batch of npz files to fits')
 a.add_argument('files', metavar='files', type=str, nargs='*', default=[],
                help='*.npz files to convert to fits.')
+args = a.parse_args()
 
-for f in a.files:
+for f in args.files:
     d = np.load(f)
     of = f[:-3] + 'fits'
     epic2fits(of, d['image'], np.ravel(d['hdr'])[0], d['image_nums'])
